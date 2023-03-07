33c59d3452be41d7fd0ec892055e2e32662052c2
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2023-03-07 17:03:52 -0300

    finish car card

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { StatusBar } from "react-native";
-import { Container, Header, HeaderContent, TotalCars } from "./styles";
+import { CarList, Container, Header, HeaderContent, TotalCars } from "./styles";
 
 import Logo from "../assets/logo.svg";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Car } from "../components/Car";
 
 export function Home() {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail:
+      "https://www.audicentersalvador.com.br/assets/uploads/nt_veiculos/11459-Imagem-topo_-597-x-340.png?v=2",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -20,6 +32,12 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 6]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({}) => <Car data={carData} />}
+      />
     </Container>
   );
 }
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -29,9 +29,9 @@ export const TotalCars = styled.Text`
   color: ${({ theme }) => theme.colors.text};
 `;
 
-// export const CarList = styled(FlatList as new () => FlatList<Car>).attrs({
-//   contentContainerStyle: {
-//     padding: 24,
-//   },
-//   showsVerticalScrollIndicator: false,
-// })``;
+export const CarList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 24,
+  },
+  showsVerticalScrollIndicator: false,
+})``;
