0ff0dbdf6f96d2e149cf3cf910b8c06cc95f6586
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2023-03-07 18:16:28 -0300

    create header of carDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
-import { Container, Header } from "./styles";
+import { ImageSlider } from "../../components/ImageSlider";
+import { CarImages, Container, Header } from "./styles";
 
 export function CarDetails() {
   function handleGoBack() {}
@@ -10,6 +11,13 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={handleGoBack} />
       </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://www.audicentersalvador.com.br/assets/uploads/nt_veiculos/11459-Imagem-topo_-597-x-340.png?v=2",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
